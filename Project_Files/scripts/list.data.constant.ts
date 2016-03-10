05a46c5fc9024f101cae685136222632cb68fb14
Michaela Lehr <michaela.lehr@geildanke.com>
2016-03-10 17:59:22 +0000

    MOD use constant for bookmark default data

--- Project_Files/scripts/list.data.constant.ts
+++ Project_Files/scripts/list.data.constant.ts
@@ -1,6 +1,6 @@
 // We are using a constant here,
 // because we do not want to change the default data.
-export var BOOKMARKS = [
+export const BOOKMARKS = [
   { 'name': 'Twitter', 'url': 'https://twitter.com' },
   { 'name': 'Github', 'url': 'https://github.com' },
   { 'name': 'Sitepoint', 'url': 'https://sitepoint.com' },
